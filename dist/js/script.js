5caf869425011282a54d9c4db41353009f7dc136
Kate-M <ekaterina.minich@gmail.com>
2018-04-27 17:12:05 +0300

    feature: extended common markup and styles

--- dist/js/script.js
+++ dist/js/script.js
@@ -81,11 +81,27 @@
 "use strict";
 /* WEBPACK VAR INJECTION */(function($) {
 
-var p = $('.field-wrap');
-$.each([52, 97, 10], function (index, value) {
-    console.log(index + ": " + value);
+var he = $('main').height();
+// console.log(he)
+
+(function () {
+    $(window).scroll(appearanceButton);
+    function appearanceButton() {
+        var heidthFooter = $('footer').outerHeight();
+        var scrollHeight = $(document).height() - heidthFooter;
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+
+        if (Math.round(scrollPosition) >= scrollHeight) {
+            $('section.controls-task-secondary').removeClass('fixed');
+        } else {
+            $('section.controls-task-secondary').addClass('fixed');
+        }
+    }
+})();
+
+$('.menu-btn').click(function () {
+    $('.controls-task-main').toggleClass('open');
 });
-console.log(p + 'all work');
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
 
 /***/ }),
